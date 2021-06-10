29cd5a9f0b9340bb7c535eee017ee00758a2b9be
Ncesecrede <francescopm95@gmail.com>
2021-06-10 16:34:33 +0100

    Campo Minato

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 
 //NUMERI COMPUTER
+function isInArray(array, element) {
+    // verifico con un for se l'elemento inserito come argomento
+    // è uguale al singolo item che sto analizzando dell'array durante in ciclo
+    // (in posizione x)
+    for (var x = 0; x < array.length; x++) {
+        if (element === array[x]) {
+            return true;
+        }
+    }
+    return false;
+}
 var randomNums = [];
-
+ 
 while (randomNums.length < 16){ //16 numeri
     var num = Math.floor(Math.random()* 100 + 1); //numero random da 1 a 100
-    if (num !== randomNums){ //se il numero non è presente nell'array viene pushato
+    if (isInArray(randomNums, num)){ //se il numero non è presente nell'array viene pushato
+
+    }else{
         randomNums.push(num);
     }
 }
 console.log(randomNums);
-//NUMERI UTENTE
-var userNums = [];
-
-while (userNums.length < 84) {
-
-    var userNum = prompt("inserisci un numero");
-    if (userNum === randomNums){
-        alert("the end")
-    }else {
-        userNums.push(userNum);
-    }
-}
-console.log(userNums);
-var result = userNums.length;
-document.getElementById("score").innerHTML = result;
