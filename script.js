9cbf3dd1ef7ae7756469516f7ef2d792bb8eda03
Ncesecrede <francescopm95@gmail.com>
2021-06-11 11:12:22 +0100

    Campo Minato

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+//RIFINITURA CODICE PUSHATO 11/06/2021
+
 //NUMERI COMPUTER
 function isInArray(array, element) {
     // verifico con un for se l'elemento inserito come argomento
@@ -11,35 +13,47 @@ function isInArray(array, element) {
     }
     return false;
 }
+function getRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 var randomNums = [];
 var score = document.getElementById("score");
  
 while (randomNums.length < 16){ //16 numeri
-    var num = Math.floor(Math.random()* 100 + 1); //numero random da 1 a 100
+    var num = getRandomNum(1, 100);
     if (!isInArray(randomNums, num)){ //se il numero non è presente nell'array viene pushato
         randomNums.push(num);
-    }
-        
+    }        
 }
 console.log(randomNums);
 
 //NUMERI UTENTE
 
 var userNums = [];
+var gameOver = false;
 
 while(userNums.length < 84){
     var userNum = parseInt(prompt("inserisci un numero da 1 a 100"));
-    if(isInArray(randomNums, userNum)){
-        alert("fine dei giochi");
-        break;
-    }else if(isInArray(userNums, userNum)){
-        alert("hai gia inserito il numero")
-        userNum;
+    if(!isInArray(userNums, userNum)){ //se il numero inserito non è nell'array continua.
+        
+        if(isInArray(randomNums, userNum)){
+            alert("Fine dei Giochi")
+            gameOver = true;
+            break;
+        }else{
+            userNums.push(userNum);
+        }
+        
     }else{
-        userNums.push(userNum);
+        alert("il numero è gia stato inserito!!!")
     }
 }
 var result = userNums.length;
-score.innerHTML = "il tuo punteggio è: " + " " + result;
+if (!gameOver){ //se esce dal ciclo e gameover non è false vince.
+    score.innerHTML = "HAI VINTO, IL TUO PUNTEGGIO è DI: " + " " + result;
+}else{
+    score.innerHTML = "HAI PERSO CON UN PUNTEGGIO DI: " + " " + result;
+}
+
 
